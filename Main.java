93aa096ec0edc028cbc62a8ddbe1a7825009a329
Stephen Cerruti <scerruti@otabi.com>
2024-07-03 13:16:18 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    /** DECLARATION **/
+    String line1
+    int line2
+    boolean _line3
+    Object AUTHOR;
+    
+    /** INITIALIZATION **/
+
+    /** PRINTF SECTION **/
+  }
+}
